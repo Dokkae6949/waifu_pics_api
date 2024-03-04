4655d2f3be0ce2d5b50d54f28ba76e3782bee445
Dokkae6949 <finnliry@gmail.com>
2024-03-04 20:20:11 +0100

    Make error serializable

--- src/lib.rs
+++ src/lib.rs
@@ -79,6 +79,15 @@ struct RequestMany {
 }
 
 
+impl serde::Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::ser::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
+}
+
 impl Display for SfwCategory {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self)
